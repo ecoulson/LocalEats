604331f8bf9dfc5e2a1710a8a472b974dbc6f719
Evan Coulson <evan.m.coulson@gmail.com>
2021-05-10 00:50:43 -0700

    updated scroll and hover css

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const button = document.getElementsByClassName("button-mailing-list")[0];
+const mailingListSection = document.getElementById("mailing-list");
+const arrow = document.getElementsByClassName("arrow")[0];
+
+arrow.addEventListener("click", (e) => {
+  e.preventDefault();
+  mailingListSection.scrollIntoView();
+});
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  mailingListSection.scrollIntoView();
+});
+
 const SCRIPT_URL =
   "https://script.google.com/macros/s/AKfycbzGg5FjEqDAMT2jD-5G6iwC3UeamIqNlCHqyvhkgnQCQmWLtvbkwmq0cXDf8sI18bzFeQ/exec";
 const form = document.forms["mailing-list"];
